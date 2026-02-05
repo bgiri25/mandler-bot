65a3fe76140d17cb63d0866eade11d531df0cb6c
Bikash Giri <bikashgiri17@gmail.com>
2026-02-05 14:13:24 +0100

    add mandelbrot file

---
+++ mandelbrot.py
@@ -0,0 +1,17 @@
+"""
+M a n d e l b r o t Set Ge n er at o r
+Author : [ Your Name ]
+Course : Nu me r ic al S c i e n t i f i c Co mp u ti ng """
+def f ( x ) :
+	"""
+		Example function .
+		P a r a m e t e r s
+		- - - - - - - - - -
+		x : float
+		Input value
+		Returns
+		- - - - - - -
+		float
+		Output value
+	"""
+# TODO : Im pl e me nt the a lg o ri th m
